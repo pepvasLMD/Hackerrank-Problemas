7b8963f6e8b3eecba4109a9c5f4172271d66499c
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-05-04 18:00:21 -0500

    Problema terminado

---
+++ If Else/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 != 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                if (numero > 1 && numero < 6)
+                {
+                    Console.WriteLine("Not weird");
+                }
+
+                if (numero > 5 && numero < 21)
+                {
+                    Console.WriteLine("Weird");
+                }
+
+                if (numero > 20)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+            }
+        }
+    }
+}
