63c20a6399e4926dd5fd79997bc1daffe5a95397
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-05-04 15:06:27 -0500

    Problema terminado

---
+++ Stdin And Stdout 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Stdin_And_Stdout_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputInt = Convert.ToInt32(Console.ReadLine());
+            double inputDouble = Convert.ToDouble(Console.ReadLine());
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine("String: {0}", inputString);
+            Console.WriteLine("Double: {0}", inputDouble);
+            Console.WriteLine("Int: {0}", inputInt);
+
+        }
+    }
+}
