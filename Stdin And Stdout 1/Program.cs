f4099100d7b812270bb4ee3762a319f649fbc1e4
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-05-04 14:36:36 -0500

    Problema completado

---
+++ Stdin And Stdout 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Stdin_And_Stdout_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
