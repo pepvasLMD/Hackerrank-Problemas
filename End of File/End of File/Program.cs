a74e1a1a3769129dd822f75305a9828bb1fd9436
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-05-05 16:54:54 -0500

    Problema terminado

---
+++ End of File/End of File/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace End_of_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada;
+            ulong contador = 1;
+
+            do
+            {
+                entrada = Console.ReadLine();
+
+                if(!string.IsNullOrEmpty(entrada))
+                    Console.WriteLine(contador++ + " " + entrada);
+
+            }
+            while (!string.IsNullOrEmpty(entrada));
+        }
+    }
+}
