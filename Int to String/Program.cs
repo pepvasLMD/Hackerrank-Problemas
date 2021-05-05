4559077209396246ad7ce6d17c17a2ad4623a99f
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-05-05 17:16:54 -0500

    Problema terminado

---
+++ Int to String/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Int_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte n;
+
+            try
+            {
+                n = Convert.ToSByte(Console.ReadLine());
+
+                if (n >= -100 && n <= 100)
+                {
+                    Console.WriteLine("Good Job");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong answer");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Wrong answer");
+            }
+        }
+    }
+}
