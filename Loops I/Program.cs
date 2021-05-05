9a30dcb62246d1d288a060e889a3dd5ca0622b88
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-05-05 14:06:35 -0500

    Problema terminado

---
+++ Loops I/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Loops_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte i, N;
+
+            N = Convert.ToByte(Console.ReadLine());
+
+            if(N > 1 && N <= 20)
+            {
+                for (i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine("{0} x  {1} = {2}", N, i, (N * i));
+                }
+            }
+            
+        }
+    }
+}
