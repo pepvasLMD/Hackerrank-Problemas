4a189def4262de2cd3bd2f6b5f38f5fc278ad64c
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-05-06 15:31:32 -0500

    Problema terminado

---
+++ Date And Time/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Date_And_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] entrada = Console.ReadLine().Split(" ");
+            byte month = Convert.ToByte(entrada[0]);
+            byte day = Convert.ToByte(entrada[1]);
+            short year = Convert.ToInt16(entrada[2]);
+
+            Console.WriteLine(getDay(month, day, year));
+        }
+
+        static string getDay(int month, int day, int year)
+        {
+            DateTime dt = new DateTime(year, month, day);
+
+            return dt.DayOfWeek.ToString().ToUpper();
+        }
+    }
+}
