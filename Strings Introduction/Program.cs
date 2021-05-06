1b0200308f47efd9ba49c1e75306f59771636530
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-05-06 16:15:05 -0500

    Problema terminado

---
+++ Strings Introduction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Strings_Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String A = Console.ReadLine();
+            String B = Console.ReadLine();
+
+            Console.WriteLine((A.Length + B.Length));
+
+            if (A.CompareTo(B) > 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            
+            Console.WriteLine(char.ToUpper(A[0]) + A.Substring(1) + " " + char.ToUpper(B[0]) + B.Substring(1));
+        }
+    }
+}
