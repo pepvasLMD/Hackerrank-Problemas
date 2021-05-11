19722c6e31479080fb57b119f4ccfa602d679813
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-05-11 13:52:50 -0500

    Problema terminado

---
+++ Primality Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+
+namespace Primality_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger a = BigInteger.Parse(Console.ReadLine());
+            bool esPrimo = true;
+
+            if(a == 1)
+            {
+                esPrimo = false;
+            }
+            else
+            {
+                for (BigInteger b = 2; b < a; b++)
+                {
+                    if (a % b == 0)
+                    {
+                        esPrimo = false;
+                        break;
+                    }
+                }
+            }
+
+
+            if (esPrimo)
+            {
+                Console.WriteLine("prime");
+            }
+            else
+            {
+                Console.WriteLine("not prime");
+            }
+        }
+    }
+}
