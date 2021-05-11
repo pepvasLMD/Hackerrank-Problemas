07e2b3f950b17ab3f53f1042be0634215b3ac507
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-05-11 14:59:22 -0500

    Problema Terminado

---
+++ 1D Array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1D_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[Convert.ToInt32(Console.ReadLine())];
+
+            for(int i=0; i< myArray.Length; i++)
+            {
+                myArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for(int i=0; i<myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
