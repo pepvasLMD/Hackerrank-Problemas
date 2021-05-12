81bf6a941679503ba9f42a764ef7c066ce4f5956
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-05-12 16:24:52 -0500

    Problema Terminado

---
+++ BigInteger/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+
+namespace BigIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger a = BigInteger.Parse(Console.ReadLine());
+            BigInteger b = BigInteger.Parse(Console.ReadLine());
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a * b);
+        }
+    }
+}
