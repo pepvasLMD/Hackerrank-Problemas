3b8def5d40fafb299788caafe1474358d738aedc
Eduardo <Eduardo@DESKTOP-V81QGSK>
2021-05-12 16:20:53 -0500

    Problema Terminado

---
+++ Output Formatting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Output_Formatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] entradas = new string[3][];
+
+            entradas[0] = Console.ReadLine().Split(" ");
+            entradas[1] = Console.ReadLine().Split(" ");
+            entradas[2] = Console.ReadLine().Split(" ");
+
+            for (int i =0; i<entradas.Length; i++)
+            {
+                for(int j=entradas[i][0].Length; j<15; j++)
+                {
+                    entradas[i][0] += " ";
+                }
+
+                for(int j=entradas[i][1].Length; j<3; j++)
+                {
+                    entradas[i][1] = "0" + entradas[i][1];
+                }
+
+                Console.WriteLine(entradas[i][0] + entradas[i][1]);
+            }
+
+        }
+    }
+}
